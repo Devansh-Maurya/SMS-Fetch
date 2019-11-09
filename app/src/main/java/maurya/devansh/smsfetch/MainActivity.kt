c414fe39f0baf9d03c2ef916669b876e8b46a3fb
Devansh-Maurya <devansh233@gmail.com>
2019-11-09 19:45:25 +0530

    Asking for SMS read permission using TedPermissions library

--- app/src/main/java/maurya/devansh/smsfetch/MainActivity.kt
+++ app/src/main/java/maurya/devansh/smsfetch/MainActivity.kt
@@ -1,12 +1,38 @@
 package maurya.devansh.smsfetch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    val firestore = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                toast("Permission granted")
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String>) {
+                toast("Permission denied")
+            }
+        }
+
+        TedPermission.with(this)
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("If you reject permission,you can not use this service\n\n" +
+                    "Please turn on permissions at [Setting] > [Permission]")
+            .setPermissions(Manifest.permission.READ_SMS)
+            .check()
     }
+
 }
